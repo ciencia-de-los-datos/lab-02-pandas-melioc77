69db1466d882de8744a9e9df24981357b755aae1
melioc77 <121962019+melioc77@users.noreply.github.com>
2024-04-09 19:43:27 -0500

    2

--- preguntas.py
+++ preguntas.py
@@ -22,7 +22,9 @@ def pregunta_01():
     40
 
     """
-    return
+    a = tbl0.shape[0]
+    return a
+
 
 
 def pregunta_02():
@@ -33,7 +35,8 @@ def pregunta_02():
     4
 
     """
-    return
+    a = tbl0.shape[1]
+    return a
 
 
 def pregunta_03():
@@ -50,9 +53,12 @@ def pregunta_03():
     Name: _c1, dtype: int64
 
     """
-    return
 
+    #series = pd.Series(a, name='_c1')
+    #tbl0.groupby("_c1").count
+    return tbl0["_c1"].value_counts().sort_index()
 
+#print(pregunta_03())
 def pregunta_04():
     """
     Calcule el promedio de _c2 por cada letra de la _c1 del archivo `tbl0.tsv`.
@@ -65,7 +71,7 @@ def pregunta_04():
     E    4.785714
     Name: _c2, dtype: float64
     """
-    return
+    return tbl0.groupby("_c1")["_c2"].mean().sort_index()
 
 
 def pregunta_05():
@@ -82,7 +88,7 @@ def pregunta_05():
     E    9
     Name: _c2, dtype: int64
     """
-    return
+    return tbl0.groupby("_c1")["_c2"].max().sort_index()
 
 
 def pregunta_06():
@@ -94,7 +100,8 @@ def pregunta_06():
     ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 
     """
-    return
+
+    return  tbl1["_c4"].str.upper().sort_values().unique().tolist()
 
 
 def pregunta_07():
@@ -110,7 +117,8 @@ def pregunta_07():
     E    67
     Name: _c2, dtype: int64
     """
-    return
+    return tbl0.groupby("_c1")["_c2"].sum().sort_index()
+   
 
 
 def pregunta_08():
@@ -128,7 +136,8 @@ def pregunta_08():
     39   39   E    5  1998-01-26    44
 
     """
-    return
+    a = tbl0["_c0"] + tbl0["_c2"]
+    return tbl0.assign(suma=a)
 
 
 def pregunta_09():
@@ -146,7 +155,8 @@ def pregunta_09():
     39   39   E    5  1998-01-26  1998
 
     """
-    return
+
+    return tbl0.assign(year=tbl0["_c3"].str.split("-").str[0])
 
 
 def pregunta_10():
@@ -163,9 +173,12 @@ def pregunta_10():
     3   D                  1:2:3:5:5:7
     4   E  1:1:2:3:3:4:5:5:5:6:7:8:8:9
     """
-    return
-
-
+    table=tbl0.groupby("_c1")["_c2"].apply(lambda x: ":".join(map(str, sorted(list(x)))))
+    df = pd.DataFrame(table)
+    
+    return df
+#print(pregunta_10())
+#print(pregunta_10())
 def pregunta_11():
     """
     Construya una tabla que contenga _c0 y una lista separada por ',' de los valores de
@@ -182,8 +195,10 @@ def pregunta_11():
     38   38      d,e
     39   39    a,d,f
     """
-    return
-
+    table = tbl1.groupby("_c0")["_c4"].apply(lambda x: ",".join(sorted(x)))
+    table = table.reset_index()
+    return table
+#print(pregunta_11())
 
 def pregunta_12():
     """
@@ -200,8 +215,9 @@ def pregunta_12():
     38   38                    eee:0,fff:9,iii:2
     39   39                    ggg:3,hhh:8,jjj:5
     """
-    return
-
+    
+    return tbl2.groupby("_c0").apply(lambda x: ",".join(sorted(x["_c5a"] + ":" + x["_c5b"].astype(str)))).reset_index().rename(columns={0:"_c5"})
+#print(pregunta_12())
 
 def pregunta_13():
     """
@@ -217,4 +233,8 @@ def pregunta_13():
     E    275
     Name: _c5b, dtype: int64
     """
-    return
+    tbl = tbl0[["_c0", "_c1"]]
+    table = pd.merge(tbl, tbl2, sort=True)
+    table = table.groupby("_c1")["_c5b"].sum()
+    return table
+#print(pregunta_13())
